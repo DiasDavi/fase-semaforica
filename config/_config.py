c215daa2d3c3d959cb95c5ed5a33b80ab613d767
DiasDavi <davi.dias271@gmail.com>
2025-04-10 23:58:55 -0300

    feat: adiciona as configuracoes

---
+++ config/_config.py
@@ -0,0 +1,10 @@
+DETECT_MODEL_PATH = "models/detect.pt"
+CLASSIFIER_MODEL_PATH = "models/classifier.h5"
+
+DETECTION_IMG_SIZE = (640, 640)
+CLASSIFICATION_IMG_SIZE = (150, 150)
+
+DETECTOR_CONFIG_YAML = "config/detector.yaml"
+CLASSIFIER_CONFIG_YAML = "config/classifier.yaml"
+
+CLASS_NAMES = ["amarelo", "verde", "vermelho"]  
